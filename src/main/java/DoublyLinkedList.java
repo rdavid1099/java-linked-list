883e3b5dfa529739c3115a937e46ef6abe4db9d9
Ryan Workman <rdavid1099@gmail.com>
2017-12-05 14:16:58 -0700

    Build out base LinkedList and Node

--- src/main/java/DoublyLinkedList.java
+++ src/main/java/DoublyLinkedList.java
@@ -3,9 +3,30 @@ package main.java;
 import main.java.Node;
 
 public class DoublyLinkedList<T> {
-  Node<T> initialNode;
+  private Node<T> initialNode;
+  private int count;
 
   public DoublyLinkedList() {
+    this.count = 0;
+  }
 
+  public void push(T data) {
+    Node<T> newNode = new Node<>(data);
+    if (this.count == 0) {
+      this.initialNode = newNode;
+    } else {
+      Node<T> lastNode = getLastNode(this.initialNode);
+      lastNode.assignNext(newNode);
+      newNode.assignPrev(lastNode);
+    }
+    this.count++;
+  }
+
+  public Node<T> getLastNode(Node<T> currentNode) {
+    if (currentNode.hasNext()) {
+      return getLastNode(currentNode.getNext());
+    } else {
+      return currentNode;
+    }
   }
 }
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -2,5 +2,37 @@ package main.java;
 
 public class Node<T> {
   private T data;
-  
+  private Node<T> nextNode, prevNode;
+
+  public Node(T data) {
+    this.data = data;
+  }
+
+  public void assignNext(Node<T> nextNode) {
+    this.nextNode = nextNode;
+  }
+
+  public void assignPrev(Node<T> prevNode) {
+    this.prevNode = prevNode;
+  }
+
+  public T getData() {
+    return this.data;
+  }
+
+  public Node<T> getNext() {
+    return this.nextNode;
+  }
+
+  public Node<T> getPrev() {
+    return this.prevNode;
+  }
+
+  public boolean hasNext() {
+    return this.nextNode == null;
+  }
+
+  public boolean hasPrev() {
+    return this.prevNode == null;
+  }
 }
