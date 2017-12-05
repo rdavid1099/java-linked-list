6444c1412b0bbbba50a7b7c2fb61add34c6d7a28
Ryan Workman <rdavid1099@gmail.com>
2017-12-05 11:23:00 -0700

    Add node class

---
+++ src/main/java/DoublyLinkedList.java
@@ -0,0 +1,11 @@
+package main.java;
+
+import main.java.Node;
+
+public class DoublyLinkedList<T> {
+  Node<T> initialNode;
+
+  public DoublyLinkedList() {
+
+  }
+}
---
+++ src/main/java/Node.java
@@ -0,0 +1,6 @@
+package main.java;
+
+public class Node<T> {
+  private T data;
+  
+}
